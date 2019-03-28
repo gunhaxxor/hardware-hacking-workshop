00dbcdca1e04f310ef8914ec3930a971b2605b59
HACKING-STATION\gunhaxxor <gunnar.oledal@gmail.com>
2019-03-28 10:17:47 +0100

    Fixade en bugg i serial-exemplet

--- 03-serial/03-serial.ino
+++ 03-serial/03-serial.ino
@@ -2,10 +2,7 @@
 
 int counter = 0;
 void setup (){
-  //pullup means that the pin internally gets connected to HIGH through a 20Mohm resistor.
-  //The button should then get connected between the pin and GND.
-  //This means that digitalRead returns LOW (GND) when the button is pressed.
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
